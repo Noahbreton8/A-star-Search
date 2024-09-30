26a8cf3a0cc2207d7733305bea41fbebd114b4dd
marcobt15 <mtoito03@gmail.com>
2024-09-30 13:27:42 -0400

    finished

--- assignment1.py
+++ assignment1.py
@@ -2,9 +2,10 @@ import heapq #in standard library
 import copy #in standard library
 import pandas as pd
 
+#This class stores the information that will be used in the frontier
 class State:
     def __init__(self, grid, heuristic, curr_location, num_keys = 0, path_cost = 0, path = []):
-        self.path = path + [curr_location]
+        self.path = path + [curr_location] #Stores the path from start to itself
         self.h = heuristic
         self.path_cost = path_cost
         self.grid = grid
@@ -14,23 +15,21 @@ class State:
     def getPriorityValue(self):
         return self.path_cost + self.h
     
+    #This is used for equality of two states
     def __eq__(self, other):
         return self.location == other.location and self.num_keys == other.num_keys
     
+    #This is used for the order of the priority queue
     def __lt__(self, other):
-        # Uncomment if we want to order by heuristic (distance to goal) when they're tied
-        # if self.getPriorityValue() == other.getPriorityValue():
-        #     return self.h < other.h
+        if self.getPriorityValue() == other.getPriorityValue():
+            return self.h < other.h
         return self.getPriorityValue() < other.getPriorityValue()
     
-    #for debugging purposes
-    def __str__(self):
-        return f"My heuristic is {self.h}, my path cost is {self.path_cost}, my path is {self.path}"
-
 #Returns the grid from the csv file as a 2D array
 def readFile(filepath):
     return pd.read_csv(filepath, header=None).values
 
+#Retrieves the start and goal positions from the grid
 def findStartAndGoal(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
@@ -41,22 +40,31 @@ def findStartAndGoal(grid):
 
     return start, goal
 
+#Returns the heuristic given the current position and goal
+#The heuristic chosen is manhattan distance
 def getHeuristic(curr_position, goal):
     return abs(curr_position[0] - goal[0]) + abs(curr_position[1] - goal[1])
 
+#Checks if a point exists on the grid
 def valid_point(point, row_length, col_length):
     if point[0] < 0 or point[1] < 0 or point[0] >= row_length or point[1] >= col_length:
         return False
     return True
 
+#Gets the valid neighbours of a given location
 def getNeighbours(grid, curr_location, has_key):
-    directions = [(1,0), (0,1), (0,-1), (-1, 0)]
+    directions = [(1,0), (0,1), (0,-1), (-1, 0)] #The four moveable directions
 
     neighbours = []
 
     for direction in directions:
+        #adds the curr_location and direction tuples
         new_point = tuple(a + b for a, b in zip(direction, curr_location))
+
+        #checks if the new point is valid
+        #checks if the new point isn't a door, or if it a door then check if you have a key to open it
         if valid_point(new_point, len(grid), len(grid[0])) and (grid[new_point[0]][new_point[1]] != 'D' or has_key):
+            #If all checks pass, then it is a valid neighbour
             neighbours.append(new_point)
     
     return neighbours
@@ -65,7 +73,7 @@ def getNeighbours(grid, curr_location, has_key):
 # The pathfinding function must implement A* search to find the goal state
 def pathfinding(filepath):
     start_grid = readFile(filepath)
-    start, goal = findStartAndGoal(start_grid)
+    start_pos, goal_pos = findStartAndGoal(start_grid)
 
     # optimal_path is a list of coordinate of squares visited (in order)
     optimal_path = []
@@ -74,63 +82,63 @@ def pathfinding(filepath):
     # num_states_explored is the number of states explored during A* search
     num_states_explored = 0
 
-    start_heuristic = getHeuristic(start, goal)
-    start_state = State(start_grid, start_heuristic, start)
+    start_heuristic = getHeuristic(start_pos, goal_pos)
+    start_state = State(start_grid, start_heuristic, start_pos)
 
-    #basic graph search from lecture, change it to use heuristics and other
     frontier = [start_state]
     explored = set() #for easy lookup
 
     while True:
+        #There should be a solution
         if frontier == []:
-            #there should be a solution
-            return False
+            return [], -1, -1
         
         #use heapq to pop off best priority
-        #leaf = State object
-        leaf = heapq.heappop(frontier)
+        curr_state = heapq.heappop(frontier)
         num_states_explored += 1
 
-        if (leaf.location) == goal:
-            optimal_path_cost = leaf.path_cost
-            optimal_path = leaf.path
+        if curr_state.location == goal_pos:
+            #curr_state stores the path cost and optimal path
+            optimal_path_cost = curr_state.path_cost
+            optimal_path = curr_state.path
             break
 
-        explored.add(leaf.location + (leaf.num_keys, ))
+        # (x, y) + (num_keys, ) = (x, y, num_keys), easy way to concatenate tuples
+        explored.add(curr_state.location + (curr_state.num_keys, ))
 
-        curr_grid = leaf.grid
+        curr_grid = curr_state.grid
 
-        for node in getNeighbours(leaf.grid, leaf.location, leaf.num_keys > 0):
+        for node in getNeighbours(curr_state.grid, curr_state.location, curr_state.num_keys > 0):
             
             new_grid = copy.deepcopy(curr_grid)
             new_grid[node[0]][node[1]] = 'O'
 
-            new_heuristic = getHeuristic(node, goal)
+            new_heuristic = getHeuristic(node, goal_pos)
             
-            curr_keys = leaf.num_keys
+            curr_keys = curr_state.num_keys
             if curr_grid[node[0]][node[1]] == 'K':
                 curr_keys += 1
             elif curr_grid[node[0]][node[1]] == 'D':
                 curr_keys -= 1
             
-            new_path_cost = leaf.path_cost + 1 #weight between two nodes will always be one
+            new_path_cost = curr_state.path_cost + 1 #weight between two nodes will always be one
             
-            new_state = State(new_grid, new_heuristic, node, curr_keys, new_path_cost, leaf.path)
+            new_state = State(new_grid, new_heuristic, node, curr_keys, new_path_cost, curr_state.path)
 
+            #Checking for repeat states
             same_state = False
             for state in frontier:
                 if new_state == state and new_path_cost < state.path_cost:
                     same_state = True
                     del new_state
-                    new_state = state
-                    #Since the path we're taking right now is best update the path cost and path
+                    new_state = copy.deepcopy(state)
+                    #Since the path we're taking right now is better, update the path cost and path
                     new_state.path_cost = new_path_cost 
-                    new_state.path = leaf.path + [node]
+                    new_state.path = curr_state.path + [node]
                     break
-
+            
+            #If we've taken from the frontier or the new_state isn't in the frontier and isn't in explored
             if same_state or (new_state not in frontier and node + (curr_keys, ) not in explored):
                 heapq.heappush(frontier, new_state)
 
     return optimal_path, optimal_path_cost, num_states_explored
-
-print(pathfinding('Examples/Question4/grid.csv'))
